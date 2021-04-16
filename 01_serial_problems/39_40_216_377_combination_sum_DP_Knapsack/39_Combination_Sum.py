7f93f62a8a0877442de7c776dbfff58caf5b0350
letaoZ <letaogithub@gmail.com>
2021-04-16 14:46:20 -0500

    knapsack series

--- 01_serial_problems/39_40_216_377_combination_sum_DP_Knapsack/39_Combination_Sum.py
+++ 01_serial_problems/39_40_216_377_combination_sum_DP_Knapsack/39_Combination_Sum.py
@@ -1,10 +1,43 @@
 #%%
+'''
+39. Combination Sum
+Medium
 
+
+Example 1:
+
+Input: candidates = [2,3,6,7], target = 7
+Output: [[2,2,3],[7]]
+Explanation:
+2 and 3 are candidates, and 2 + 2 + 3 = 7. Note that 2 can be used multiple times.
+7 is a candidate, and 7 = 7.
+These are the only two combinations.
+Example 2:
+
+Input: candidates = [2,3,5], target = 8
+Output: [[2,2,2,2],[2,3,3],[3,5]]
+Example 3:
+
+Input: candidates = [2], target = 1
+Output: []
+Example 4:
+
+Input: candidates = [1], target = 1
+Output: [[1]]
+Example 5:
+
+Input: candidates = [1], target = 2
+Output: [[1,1]]
+ 
+
+Constraints:
+
+1 <= candidates.length <= 30
+1 <= candidates[i] <= 200
+All elements of candidates are distinct.
+1 <= target <= 500
+'''
 import collections
-
-
-
-#%%
 class Solution:
     def combinationSum_uselist(self, candidates, target: int) -> [[int]]:
         
