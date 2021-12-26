08d3da264624e5315beae9eff04fd4bc94dfeaf8
letaoZ <letaogithub@gmail.com>
2021-12-25 20:10:34 -0500

    time complexity

--- DP/dp7d_343_Integer_Break.py
+++ DP/dp7d_343_Integer_Break.py
@@ -32,9 +32,8 @@ Constraints:
 2 <= n <= 58
 '''
 
-
 class Solution:
-    def integerBreak(self, n: int) -> int:
+    def integerBreak_bottomUP(self, n: int) -> int:
         if n <= 2:
             return 1
         
@@ -43,6 +42,7 @@ class Solution:
         dp[1] = 1
         dp[2] = 2
         
+        ## time = O(num)
         def max_prod(num):
             if dp[num] > 0:
                 return dp[num]
@@ -53,7 +53,23 @@ class Solution:
             dp[num] = res
             return res
         
+        
+        ## time:= sum (O(num) ), num = 1,...n --> O(n**2)
         for i in range(1,n//2+1):
             res = max(res, max_prod(i)*max_prod(n-i))
             
         return res
+
+    def integerBreak(self, n: int) -> int:
+        if n<=2:
+            return 1
+        
+        ##dp[k] max prod you get if sum = k
+        ## dp[k] = max(dp[k-i]*dp[i]) for i in range(1,k//2 + 1)
+        dp = [0 for k in range(n+1)]
+        dp[1] = dp[2] = 1
+        
+        for k in range(3, n +1):
+            for i in range(1, k//2 + 1):
+                dp[k] = max(dp[k],max(i,dp[i]) * max(k-i, dp[k-i]) )
+        return dp[n]
