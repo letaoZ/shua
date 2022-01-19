aad2a079a6ef9bba130574b90f33832a86d88c6e
letaoZ <letaogithub@gmail.com>
2022-01-19 15:12:46 -0500

    update

---
+++ Graph_Search/47_Permutations_II.py
@@ -0,0 +1,58 @@
+'''
+47. Permutations II
+Medium
+
+4288
+
+88
+
+Add to List
+
+Share
+Given a collection of numbers, nums, that might contain duplicates, return all possible unique permutations in any order.
+
+ 
+
+Example 1:
+
+Input: nums = [1,1,2]
+Output:
+[[1,1,2],
+ [1,2,1],
+ [2,1,1]]
+Example 2:
+
+Input: nums = [1,2,3]
+Output: [[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]
+ 
+
+Constraints:
+
+1 <= nums.length <= 8
+-10 <= nums[i] <= 10
+'''
+
+class Solution:
+    def permuteUnique(self, nums: List[int]) -> List[List[int]]:
+        res = set()
+        N = len(nums)
+        def dfs(candidates, trace):
+            
+            if len(trace) == N:
+                res.add(tuple([_ for _ in trace]))
+                return
+            
+            
+            for ic, v in enumerate(candidates):
+                trace.append(v)
+                dfs(candidates[:ic] + candidates[ic+1:], trace)
+                trace.pop()
+                
+                
+        dfs(nums, [])
+        res = [list(_) for _ in res]
+                
+        return res
+                
+                
+                
---
+++ Graph_Search/90_Subsets_II.py
@@ -0,0 +1,59 @@
+'''
+90. Subsets II
+Medium
+
+4165
+
+134
+
+Add to List
+
+Share
+Given an integer array nums that may contain duplicates, return all possible subsets (the power set).
+
+The solution set must not contain duplicate subsets. Return the solution in any order.
+
+ 
+
+Example 1:
+
+Input: nums = [1,2,2]
+Output: [[],[1],[1,2],[1,2,2],[2],[2,2]]
+Example 2:
+
+Input: nums = [0]
+Output: [[],[0]]
+ 
+
+Constraints:
+
+1 <= nums.length <= 10
+-10 <= nums[i] <= 10
+'''
+class Solution:
+    def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
+        res = set()
+        nums.sort() ## use sort to avoid future duplicates
+        def dfs(nums,i,trace):
+            
+            # new_trace = [_ for _ in trace]
+            # new_trace.sort()
+            res.add(tuple(trace))
+            # del new_trace
+            
+            
+            if i >= len(nums):
+                return
+            
+            
+            for j in range(i,len(nums)):
+                trace.append(nums[j])
+                dfs(nums,j+1,trace)
+                trace.pop()
+                
+        dfs(nums,0,[])        
+        res = [list(_) for _ in res]
+        
+        return res
+                
+                
