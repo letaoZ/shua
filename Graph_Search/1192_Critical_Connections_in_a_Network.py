ae9ffcb5fa4233404159915be121edbd6588fc5b
letaoZ <letaogithub@gmail.com>
2022-02-08 10:59:20 -0500

    update

--- Graph_Search/1192_Critical_Connections_in_a_Network.py
+++ Graph_Search/1192_Critical_Connections_in_a_Network.py
@@ -40,39 +40,60 @@ n - 1 <= connections.length <= 105
 ai != bi
 There are no repeated connections.
 '''
-
 class Solution:
     def criticalConnections(self, n: int, connections: List[List[int]]) -> List[List[int]]:
-        ## if an edge is on a cycle,it is safe
         
-        G = collections.defaultdict(set)
-        noncritical_edges = collections.defaultdict(int)
-        for i,(a, b) in enumerate(connections):
-            G[a].add(b)
-            G[b].add(a)
-            if a<b:
-                noncritical_edges[(a,b)] = 0
-            else:
-                noncritical_edges[(b,a)] = 0
-        rk = [-2]*n ## -2 means never been visited. we use this number to distinguish depth values 
-        def dfs(v_from, v,rk_v, rk):
-            # print(f"dfs from {v_from},to {v}")
-            if rk[v]>=0:
-                return rk[v]
-            rk[v] = rk_v
-            for w in G[v]:
-                if w==v_from:
-                    continue
-                rk_w = dfs(v,w,rk_v+1,rk)
-                if rk_w <= rk_v:
-                    # print(f"rk_w <= rk_v: v={v} rk {rk_v}, w={w} rk {rk_w}, ")
-                    # print()
-                    if w<v:
-                        noncritical_edges[(w,v)] = 1
-                    else:
-                        noncritical_edges[(v,w)] = 1
-                    rk[v]  = min(rk[v] ,rk_w)
-            return rk[v] 
-        dfs(0,0,0,rk)
-        # print(rk)
-        return [[a,b] for (a,b),v in noncritical_edges.items() if v==0]
+        
+        ## if every pair of vertices have an edge then no critical connection
+        if n>2 and len(connections) >= n*(n-1)//2:
+            return []
+        if n==2:
+            return connections
+        
+        graph = [[] for _ in range(n)] ## vertex i ==> [its neighbors]
+		
+		
+        lowestRank = [2*n for i in range(n)] ## here lowestRank[i] represents the lowest order of vertex that can reach this vertex i; max could be n; so we initiate it with n+1
+		
+        
+        ## build graph:
+        for connection in connections:
+            ## this step is straightforward, build graph as you would normally do
+            graph[connection[0]].append(connection[1])
+            graph[connection[1]].append(connection[0])
+        
+        res = []
+        
+        
+        
+        prevVertex = -1 ## This -1 a dummy. Does not really matter in the beginning. 
+		## It will be used in the following DFS because we need to know where the current DFS level comes from. 
+		## You do not need to setup this parameter, I setup here ONLY because it is more clear to see what are passed on in the DFS method.
+        currentRank = 0 ## please note this rank is NOT the num (name) of the vertex itself, it is the order of your DFS level
+        currentVertex = 0 ## we start the DFS from vertex num 0 (its rank is also 0 of course)
+        self._dfs(res, graph,n, lowestRank, currentRank, prevVertex, currentVertex)
+        return res
+    
+    def _dfs(self, res, graph,n, lowestRank, currentRank, prevVertex, currentVertex):
+
+        if lowestRank[currentVertex] < 2*n:
+            return
+        
+        
+        lowestRank[currentVertex] = currentRank
+        for nextVertex in graph[currentVertex]:
+            if nextVertex == prevVertex:
+                continue ## do not include the the incoming path to this vertex since this is the possible ONLY bridge (critical connection) that every vertex needs.
+
+            # if not visited[nextVertex]:
+            self._dfs(res, graph, n, lowestRank, currentRank + 1, currentVertex, nextVertex)
+				# We avoid visiting visited nodes here instead of doing it at the beginning of DFS - 
+				# the reason is, even that nextVertex may be visited before, we still need to update my lowestRank using the visited vertex's information.
+
+            lowestRank[currentVertex] = min(lowestRank[currentVertex], lowestRank[nextVertex]) 
+			# take the min of the current vertex's and next vertex's ranking
+            if lowestRank[nextVertex] >= currentRank + 1: ####### if all the neighbors lowest rank is higher than mine + 1, then it means I am one connecting critical connection ###
+                res.append([currentVertex, nextVertex])
+
+
+                
