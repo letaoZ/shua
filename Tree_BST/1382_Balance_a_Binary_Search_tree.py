68be9d6ae149ba8b5f14fb9a0eb90a36ab91bb17
letaoZ <letaogithub@gmail.com>
2022-03-05 17:32:29 -0500

    update

---
+++ Tree_BST/1382_Balance_a_Binary_Search_tree.py
@@ -0,0 +1,78 @@
+'''
+1382. Balance a Binary Search Tree
+Medium
+
+1641
+
+54
+
+Add to List
+
+Share
+Given the root of a binary search tree, return a balanced binary search tree with the same node values. If there is more than one answer, return any of them.
+
+A binary search tree is balanced if the depth of the two subtrees of every node never differs by more than 1.
+
+ 
+
+Example 1:
+
+
+Input: root = [1,null,2,null,3,null,4,null,null]
+Output: [2,1,3,null,null,null,4]
+Explanation: This is not the only correct answer, [3,1,4,null,2] is also correct.
+Example 2:
+
+
+Input: root = [2,1,3]
+Output: [2,1,3]
+ 
+
+Constraints:
+
+The number of nodes in the tree is in the range [1, 104].
+1 <= Node.val <= 105
+
+'''
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def balanceBST(self, root: TreeNode) -> TreeNode:
+        
+        ## get an array sorted array first, then reconstruct a balanced tree
+        
+        arr = []
+        
+        def build_arr(node):
+            
+            if node is None:
+                return
+            
+            build_arr(node.left)
+            arr.append(node.val)
+            build_arr(node.right)
+            
+        build_arr(root)
+
+        ## arr is sorted, then we build a balanced binary search tree
+        
+        l, r = 0, len(arr)-1
+        
+        def build_tree(l,r):
+            if l>r:
+                return None
+            mid = l + (r-l) // 2
+            node = TreeNode(arr[mid])
+            node.left = build_tree(l,mid-1)
+            node.right = build_tree(mid+1,r)
+            return node
+        
+        rt = build_tree(l,r)
+        return rt
+    
+            
+            
