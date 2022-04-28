5cb514ca8a57febf23848c9dd5ebda513ccad14b
letaoZ <letaogithub@gmail.com>
2022-04-28 13:30:44 -0400

    update

--- Graph_Search/back_tracking/47_Permutations_II.py
+++ Graph_Search/back_tracking/47_Permutations_II.py
@@ -32,6 +32,32 @@ Constraints:
 -10 <= nums[i] <= 10
 '''
 
+## idea: maintain a relative order of nums: e.g. [1,2,2,2], then assume the "2nd" 2 is actually 2'. 3rd 2 is 2''
+## Then we must maintain 2' showup before 2'' and 2 shows up before 2' -- > SO we wont' get duplicate cases
+class Solution:
+    def permuteUnique(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()
+        res = []
+        visited = [0]*len(nums)
+        def dfs(nums,trace,res):
+            
+            if len(trace) == len(nums):
+                res.append([_ for _ in trace])
+                return
+            
+            for j in range(0, len(nums)):
+                if visited[j]:
+                    continue
+                if j>0 and nums[j] == nums[j-1] and (not visited[j-1]):
+                    continue
+                visited[j] = 1
+                dfs(nums, trace + [nums[j]],res)
+                visited[j] = 0
+                
+        dfs(nums,[],res)
+                
+        return res
+            
 
 class Solution:
     def permuteUnique(self, nums: List[int]) -> List[List[int]]:
