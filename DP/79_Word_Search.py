25f542cb559edc8b71d8fc98c5907b1c9fcab715
letaoZ <letaogithub@gmail.com>
2022-07-11 20:49:38 -0400

    update

--- DP/79_Word_Search.py
+++ DP/79_Word_Search.py
@@ -1,14 +1,6 @@
 '''
 79. Word Search
 Medium
-
-9904
-
-378
-
-Add to List
-
-Share
 Given an m x n grid of characters board and a string word, return true if word exists in the grid.
 
 The word can be constructed from letters of sequentially adjacent cells, where adjacent cells are horizontally or vertically neighboring. The same letter cell may not be used more than once.
